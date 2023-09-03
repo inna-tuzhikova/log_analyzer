55c4c1d5137e2da2e01fad046cc53e64b1460090
inna-tuzhikova <inna-tuzhikova@github.com>
2023-09-03 10:17:37 +0300

    poker

--- optional_tasks/poker.py
+++ optional_tasks/poker.py
@@ -3,21 +3,23 @@
 
 # -----------------
 # Реализуйте функцию best_hand, которая принимает на вход
-# покерную "руку" (hand) из 7ми карт и возвращает лучшую
+# покерную 'руку' (hand) из 7ми карт и возвращает лучшую
 # (относительно значения, возвращаемого hand_rank)
-# "руку" из 5ти карт. У каждой карты есть масть(suit) и
+# 'руку' из 5ти карт. У каждой карты есть масть(suit) и
 # ранг(rank)
 # Масти: трефы(clubs, C), пики(spades, S), червы(hearts, H), бубны(diamonds, D)
-# Ранги: 2, 3, 4, 5, 6, 7, 8, 9, 10 (ten, T), валет (jack, J), дама (queen, Q), король (king, K), туз (ace, A)
-# Например: AS - туз пик (ace of spades), TH - дестяка черв (ten of hearts), 3C - тройка треф (three of clubs)
+# Ранги: 2, 3, 4, 5, 6, 7, 8, 9, 10 (ten, T), валет (jack, J), дама (queen, Q),
+# король (king, K), туз (ace, A)
+# Например: AS - туз пик (ace of spades), TH - десятка черв (ten of hearts),
+# 3C - тройка треф (three of clubs)
 
-# Задание со *
+# Задание со *.
 # Реализуйте функцию best_wild_hand, которая принимает на вход
-# покерную "руку" (hand) из 7ми карт и возвращает лучшую
+# покерную 'руку' (hand) из 7ми карт и возвращает лучшую
 # (относительно значения, возвращаемого hand_rank)
-# "руку" из 5ти карт. Кроме прочего в данном варианте "рука"
+# 'руку' из пяти карт. Кроме прочего в данном варианте 'рука'
 # может включать джокера. Джокеры могут заменить карту любой
-# масти и ранга того же цвета, в колоде два джокерва.
+# масти и ранга того же цвета, в колоде два джокера.
 # Черный джокер '?B' может быть использован в качестве треф
 # или пик любого ранга, красный джокер '?R' - в качестве черв и бубен
 # любого ранга.
@@ -27,90 +29,176 @@
 # Можно свободно определять свои функции и т.п.
 # -----------------
 
+from itertools import combinations
 
-def hand_rank(hand):
+RANK_MAP = {
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'T': 10,
+    'J': 11,
+    'Q': 12,
+    'K': 13,
+    'A': 14
+}
+
+SUITS = ['C', 'S', 'H', 'D']
+
+
+def hand_rank(hand: list[str]) -> tuple:
     """Возвращает значение определяющее ранг 'руки'"""
     ranks = card_ranks(hand)
     if straight(ranks) and flush(hand):
-        return (8, max(ranks))
+        return 8, max(ranks)
     elif kind(4, ranks):
-        return (7, kind(4, ranks), kind(1, ranks))
+        return 7, kind(4, ranks), kind(1, ranks)
     elif kind(3, ranks) and kind(2, ranks):
-        return (6, kind(3, ranks), kind(2, ranks))
+        return 6, kind(3, ranks), kind(2, ranks)
     elif flush(hand):
-        return (5, ranks)
+        return 5, ranks
     elif straight(ranks):
-        return (4, max(ranks))
+        return 4, max(ranks)
     elif kind(3, ranks):
-        return (3, kind(3, ranks), ranks)
+        return 3, kind(3, ranks), ranks
     elif two_pair(ranks):
-        return (2, two_pair(ranks), ranks)
+        return 2, two_pair(ranks), ranks
     elif kind(2, ranks):
-        return (1, kind(2, ranks), ranks)
+        return 1, kind(2, ranks), ranks
     else:
-        return (0, ranks)
+        return 0, ranks
 
 
-def card_ranks(hand):
+def card_ranks(hand: list[str]) -> list[int]:
     """Возвращает список рангов (его числовой эквивалент),
     отсортированный от большего к меньшему"""
-    return
+
+    result = sorted(list(map(get_rank, hand)), reverse=True)
+    return result
 
 
-def flush(hand):
+def get_rank(card: str) -> int:
+    return RANK_MAP.get(card[0])
+
+
+def get_suit(card: str) -> str:
+    return card[1]
+
+
+def flush(hand: list[str]) -> bool:
     """Возвращает True, если все карты одной масти"""
-    return
+    suits = list(map(get_suit, hand))
+    return len(set(suits)) == 1
 
 
-def straight(ranks):
-    """Возвращает True, если отсортированные ранги формируют последовательность 5ти,
-    где у 5ти карт ранги идут по порядку (стрит)"""
-    return
+def straight(ranks: list[int]) -> bool:
+    """Возвращает True, если отсортированные ранги формируют последовательность
+    5ти, где у 5ти карт ранги идут по порядку (стрит)"""
+    return ranks == list(range(ranks[0], ranks[-1] - 1, -1))
 
 
-def kind(n, ranks):
+def kind(n: int, ranks: list[int]) -> int | None:
     """Возвращает первый ранг, который n раз встречается в данной руке.
     Возвращает None, если ничего не найдено"""
-    return
+
+    current_rank = ranks[0]
+    n_current_rank = 1
+    for r in ranks[1:]:
+        if r == current_rank:
+            n_current_rank += 1
+        else:
+            if n_current_rank == n:
+                break
+            current_rank = r
+            n_current_rank = 1
+    else:
+        if n_current_rank != n:
+            current_rank = None
+    return current_rank
 
 
-def two_pair(ranks):
-    """Если есть две пары, то возврщает два соответствующих ранга,
+def two_pair(ranks: list[int]) -> tuple[int, int] | None:
+    """Если есть две пары, то возвращает два соответствующих ранга,
     иначе возвращает None"""
-    return
+    result = None
+    first_paired_rank = kind(2, ranks)
+    if first_paired_rank is not None:
+        rest_ranks = [r for r in ranks if r != first_paired_rank]
+        second_paired_rank = kind(2, rest_ranks)
+        if second_paired_rank is not None:
+            result = first_paired_rank, second_paired_rank
+    return result
 
 
-def best_hand(hand):
-    """Из "руки" в 7 карт возвращает лучшую "руку" в 5 карт """
-    return
+def prepare_hand(hand: list[str]) -> list[str]:
+    return list(map(prepare_card, hand))
 
 
-def best_wild_hand(hand):
+def prepare_card(card: str) -> str:
+    if not isinstance(card, str):
+        raise ValueError(
+            f'Card should be a string, got: '
+            f'{card.__class__.__name__} - `{card}`'
+        )
+    card = card.upper()
+    if len(card) != 2:
+        raise ValueError(f'Card should have 2 letters, got `{card}`')
+    if card[0] not in RANK_MAP.keys():
+        raise ValueError(
+            f'Unknown rank: `{card}`. Possible ranks: {list(RANK_MAP.keys())}'
+        )
+    if card[1] not in SUITS:
+        raise ValueError(
+            f'Unknown suit: `{card}`. Possible ranks: {SUITS}'
+        )
+    return card
+
+
+def best_hand(hand: list[str]) -> list[str]:
+    """Из 'руки' в 7 карт возвращает лучшую 'руку' в 5 карт"""
+    hand = prepare_hand(hand)
+    best_score = ()
+    result = []
+    for possible_hand in combinations(hand, 5):
+        score = hand_rank(possible_hand)
+        if score > best_score:
+            best_score = score
+            result = possible_hand
+    return result
+
+
+def best_wild_hand(hand: list[str]) -> list[str]:
     """best_hand но с джокерами"""
-    return
+    hand = prepare_hand(hand)
+    return []
 
 
-def test_best_hand():
-    print("test_best_hand...")
-    assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split()))
+def test_best_hand() -> None:
+    print('test_best_hand...')
+    assert (sorted(best_hand('6C 7C 8C 9C TC 5C JS'.split()))
             == ['6C', '7C', '8C', '9C', 'TC'])
-    assert (sorted(best_hand("TD TC TH 7C 7D 8C 8S".split()))
+    assert (sorted(best_hand('TD TC TH 7C 7D 8C 8S'.split()))
             == ['8C', '8S', 'TC', 'TD', 'TH'])
-    assert (sorted(best_hand("JD TC TH 7C 7D 7S 7H".split()))
+    assert (sorted(best_hand('JD TC TH 7C 7D 7S 7H'.split()))
             == ['7C', '7D', '7H', '7S', 'JD'])
     print('OK')
 
 
-def test_best_wild_hand():
-    print("test_best_wild_hand...")
-    assert (sorted(best_wild_hand("6C 7C 8C 9C TC 5C ?B".split()))
+def test_best_wild_hand() -> None:
+    print('test_best_wild_hand...')
+    assert (sorted(best_wild_hand('6C 7C 8C 9C TC 5C ?B'.split()))
             == ['7C', '8C', '9C', 'JC', 'TC'])
-    assert (sorted(best_wild_hand("TD TC 5H 5C 7C ?R ?B".split()))
+    assert (sorted(best_wild_hand('TD TC 5H 5C 7C ?R ?B'.split()))
             == ['7C', 'TC', 'TD', 'TH', 'TS'])
-    assert (sorted(best_wild_hand("JD TC TH 7C 7D 7S 7H".split()))
+    assert (sorted(best_wild_hand('JD TC TH 7C 7D 7S 7H'.split()))
             == ['7C', '7D', '7H', '7S', 'JD'])
     print('OK')
+
 
 if __name__ == '__main__':
     test_best_hand()
-    test_best_wild_hand()
+    # test_best_wild_hand()
